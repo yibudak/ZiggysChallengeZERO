2ecf1d12cda485697b9512081357b8735028141b
Yiğit <52182279+yibudak@users.noreply.github.com>
2020-02-16 07:44:42 +0300

    Add files via upload

---
+++ Ziggy's KeyGen/Ziggy's KeyGen/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ziggy_s_KeyGen
+{
+    partial class Main
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 9);
+            this.textBox1.MaxLength = 40;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.Form1_Load);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(63, 37);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(254, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Serial";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(323, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 50);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 50);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "About";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 73);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.Text = "Ziggy\'s KeyGen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ziggy's KeyGen/Ziggy's KeyGen/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ziggy_s_KeyGen
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 5)
+            {
+                button1.Enabled = true;
+            }
+        }
+        private string Reverse(string input)
+        {
+
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            string reverseInput = new string(inputArray);
+            return reverseInput;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long magic = 1315632316;
+            char[] CopyFromArray = new char[8];
+            int a, tail = 0, head = 8;
+ 
+            byte[] name = Encoding.Default.GetBytes(Reverse(textBox1.Text));
+                var hexString = BitConverter.ToString(name);
+                hexString = hexString.Replace("-", "");
+                for (int c = 0; c < hexString.Length - 8; c++)
+                {
+                    if (hexString.Length - head < 0)
+                    {
+                        break;
+                    }
+                    a = 0;
+                    for (int i = hexString.Length - head; i < hexString.Length - tail; i++)
+                    {
+                        CopyFromArray[a] = hexString[i];
+                        a++;
+                    }
+                    
+                    tail += 2;
+                    head += 2;
+
+                    string CopyFromArrayString = new string(CopyFromArray);
+                    long decimalInput = Convert.ToInt64(CopyFromArrayString, 16);
+                    magic = decimalInput ^ magic;
+                }
+                string finalWords = magic.ToString();
+                textBox2.Text = "FIT-"+finalWords;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This KeyGen is for Ziggys KeygenMe #0 Challenge, written by yibudak.");
+        }
+    }
+}
